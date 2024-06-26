73cb8035f9533fa10ec5e42db13c60e37429b158
Rebecca Allford <rebeccaallford1@gmail.com>
2024-06-26 15:57:03 +0000

    added aullauth set up

--- flavasava/urls.py
+++ flavasava/urls.py
@@ -1,4 +1,4 @@
-from recipe.views import my_recipe
+
 
 
 """
@@ -18,12 +18,12 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+from recipe.views import my_recipe
 
 urlpatterns = [
+    path('accounts/', include("allauth.urls")),
     path('admin/', admin.site.urls),
-    path('recipe/', my_recipe, name='recipe'),
-    
-
+    path('', my_recipe, name='home'),  
    
 ]
