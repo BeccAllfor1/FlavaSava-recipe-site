3f5cec54e7312c6b9dff9ea9fd445df2b511b511
Henrr Egedegbe <egedegbehenry@gmail.com>
2024-06-27 11:18:33 +0000

    update views.py

--- recipe/views.py
+++ recipe/views.py
@@ -1,6 +1,23 @@
-from django.shortcuts import render
-from django.http import HttpResponse
-# Create your views here.
+from django.shortcuts import render, redirect, get_object_or_404
+from django.contrib.auth.decorators import login_required
+from django.contrib import messages
+from django.contrib.auth import login
+from django.db.models import Avg
+from .models import Recipe, Category, Review, Profile
+from .forms import UserRegisterForm, ProfileForm, RecipeForm, CategoryForm, ReviewForm, RecipeSearchForm
 
-def my_recipe(request):
- return HttpResponse("Hello, flavasava!")
+def home(request):
+    recipes = Recipe.objects.all().order_by('-created_at')[:6]
+    return render(request, 'home.html', {'recipes': recipes})
+
+def register(request):
+    if request.method == 'POST':
+        form = UserRegisterForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            login(request, user)
+            messages.success(request, 'Your account has been created! Please complete your profile.')
+            return redirect('create_profile')
+    else:
+        form = UserRegisterForm()
+    return render(request, 'register.html', {'form': form})
