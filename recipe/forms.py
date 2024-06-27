0345e9f06e2067e6e374d64e02008f0bbdd719d9
Henrr Egedegbe <egedegbehenry@gmail.com>
2024-06-27 11:13:36 +0000

    create forms.py

--- recipe/forms.py
+++ recipe/forms.py
@@ -15,5 +15,54 @@ class ProfileForm(forms.ModelForm):
         model = Profile
         fields = ['firstname', 'surname', 'image']
 
+class RecipeForm(forms.ModelForm):
+    class Meta:
+        model = Recipe
+        fields = ['title', 'difficulty', 'ingredients', 'cooking_time', 'serves', 'steps', 'image']
+
+    def __init__(self, *args, **kwargs):
+        super(RecipeForm, self).__init__(*args, **kwargs)
+        self.fields['cooking_time'].widget = forms.TextInput(attrs={'placeholder': 'HH:MM:SS'})
+
+class CategoryForm(forms.ModelForm):
+    class Meta:
+        model = Category
+        fields = ['name', 'description']
+
+class RecipeForm(forms.ModelForm):
+    class Meta:
+        model = Recipe
+        fields = ['title', 'difficulty', 'ingredients', 'cooking_time', 'serves', 'steps', 'image']
+
+    def __init__(self, *args, **kwargs):
+        super(RecipeForm, self).__init__(*args, **kwargs)
+        self.fields['cooking_time'].widget = forms.TextInput(attrs={'placeholder': 'HH:MM:SS'})
+
+class CategoryForm(forms.ModelForm):
+    class Meta:
+        model = Category
+        fields = ['name', 'description']
+
+class ReviewForm(forms.ModelForm):
+    class Meta:
+        model = Review
+        fields = ['title', 'rating', 'body']
+
+    def __init__(self, *args, **kwargs):
+        super(ReviewForm, self).__init__(*args, **kwargs)
+        self.fields['rating'].widget = forms.Select(choices=[(i, i) for i in range(1, 6)])
+
+class RecipeSearchForm(forms.Form):
+    search_query = forms.CharField(required=False, label='Search recipes')
+    category = forms.ModelChoiceField(
+        queryset=Category.objects.all(),
+        required=False,
+        empty_label="All Categories"
+    )
+    difficulty = forms.ChoiceField(
+        choices=[('', 'Any Difficulty')] + Recipe.DIFFICULTY_CHOICES,
+        required=False
+    )
+
 
 
