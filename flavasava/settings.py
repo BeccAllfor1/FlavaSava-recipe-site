6adc7df25d12d5eb38593c091791c1b67ae9ae93
Rebecca Allford <rebeccaallford1@gmail.com>
2024-06-28 09:08:03 +0000

    debugging pages for bootstrap

--- flavasava/settings.py
+++ flavasava/settings.py
@@ -27,7 +27,7 @@ TEMPLATES_DIR = os.path.join(BASE_DIR,'templates')
 SECRET_KEY = 'django-insecure-zl$ti*i9ih$k^*49ntbh(_0vhnwk6b8&35992d5r&p6s7fbl73'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['8000-beccallfor1-flavasavare-6f9qxe4oedo.ws.codeinstitute-ide.net',
 '.herokuapp.com']
@@ -146,8 +146,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = 'static/'
-STATICFILES_DIRS = [os.path.join(BASE_DIR,
-'static'), ]
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 # Default primary key field type
